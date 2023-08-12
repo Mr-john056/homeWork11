c544b7e8c4d9f19f9dfd640e45b820b4ffa7d35a
Mr-john056 <ekulakov056@gmail.com>
2023-08-12 09:46:10 +0500

    Сделал задачу 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void checkYear (){}
+    public static void checkYear (){
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            if (i == i + 4) {
+                System.out.println(i + " год является високосным");
+            } else System.out.println(i + " год не является високосным ");
+        }
+    }
     public static void main(String[] args) {
         task1();
         task2();
@@ -9,6 +15,7 @@ public class Main {
 
     private static void task1() {
         System.out.println("Задача 1");
+        checkYear();
     }
     private static void task2() {
         System.out.println("Задача 2");
